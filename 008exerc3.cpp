1812424235befcd5b40d507b0c1546bfeb7f36ec
Lasbernardes <lasbernardes@uesc.br>
2023-10-11 12:15:58 -0300

    v3 008exerc3.cpp

--- 008exerc3.cpp
+++ 008exerc3.cpp
@@ -36,7 +36,6 @@ int main(){
         idia=stoi(dia);
         imes=stoi(mes);
         iano=stoi(ano);
-        cout<<iano<<endl;
         switch(imes){
             case 2:{
                 if( (iano % 400 == 0) || ( (iano % 4 == 0) && (iano % 100 != 0) ) ){
@@ -60,17 +59,67 @@ int main(){
             case 8:
             case 10:
             case 12:{
-                    if(idia<1||idia>32){
+                    if(idia<1||idia>31){
                         flag=1;}
                     break;
             }
             default:{flag=true;}
         }
     }while(flag);
-    //cout<<"'"<<dia<<"'"<<endl;
-    cout<<"'"<<idia<<"'"<<endl;
-    cout<<mes<<endl;
-    cout<<ano<<endl;
-
+    cout<<idia;
+    //cout<<mes<<endl;
+    //cout<<ano<<endl;
+    switch(imes){
+            case 1:{
+                cout<<" de janeiro de ";
+                break;
+            }
+            case 2:{
+                cout<<" de fevereiro de ";
+                break;
+            }
+            case 3:{
+                cout<<" de março de ";
+                break;
+            }
+            case 4:{
+                cout<<" de abril de ";
+                break;
+            }
+            case 5:{
+                cout<<" de maio de ";
+                break;
+            }
+            case 6:{
+                cout<<" de junho de ";
+                break;
+            }
+            case 7:{
+                cout<<" de julho de ";
+                break;
+            }
+            case 8:{
+                cout<<" de agosto de ";
+                break;
+            }
+            case 9:{
+                cout<<" de setembro de ";
+                break;
+            }
+            case 10:{
+                cout<<" de outubro de ";
+                break;
+            }
+            case 11:{
+                cout<<" de novembro de ";
+                break;
+            }
+            case 12:{
+                cout<<" de dezembro de ";
+                break;
+            }
+            //default:{}
+        }
+        cout<<iano<<"."<<endl<<endl;
     return 0;
 }
